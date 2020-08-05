2136f51e71072b292c4e42270173e08416b0e853
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-08-05 02:23:04 -0300

    Not exporting ReferencePriceRepository

--- src/main/java/com/mergipe/recipesapp/ingredient/ReferencePriceRepository.java
+++ src/main/java/com/mergipe/recipesapp/ingredient/ReferencePriceRepository.java
@@ -1,6 +1,8 @@
 package com.mergipe.recipesapp.ingredient;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface ReferencePriceRepository extends CrudRepository<ReferencePrice, Long> {
 }
