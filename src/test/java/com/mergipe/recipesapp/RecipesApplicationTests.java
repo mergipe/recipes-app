9aedd26842f3220d7fe00925f9e5a4f58594d183
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-08-07 17:09:42 -0300

    Added H2 database dependency for tests

--- src/test/java/com/mergipe/recipesapp/RecipesApplicationTests.java
+++ src/test/java/com/mergipe/recipesapp/RecipesApplicationTests.java
@@ -1,9 +1,14 @@
 package com.mergipe.recipesapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class RecipesApplicationTests {
 
 	@Test
