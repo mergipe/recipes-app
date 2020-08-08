759ab114d9f5d4f16a15a867a01abb3a6f3b80ce
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-08-08 19:16:39 -0300

    Unit testing MeasurementUnitConverter

---
+++ src/test/java/com/mergipe/recipesapp/measure/MeasurementUnitConverterUnitTest.java
@@ -0,0 +1,34 @@
+package com.mergipe.recipesapp.measure;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MeasurementUnitConverterUnitTest {
+
+    private final MeasurementUnitConverter converter =
+            new MeasurementUnitConverter();
+
+    @Test
+    void testConvertToDatabaseColumn() {
+        assertThat(converter.convertToDatabaseColumn(null))
+                .isNull();
+
+        for (MeasurementUnit measurementUnit : MeasurementUnit.values()) {
+            assertThat(converter.convertToDatabaseColumn(measurementUnit))
+                    .isEqualTo(measurementUnit.name());
+        }
+    }
+
+    @Test
+    void testConvertToEntityAttribute() {
+        assertThat(converter.convertToEntityAttribute(null))
+                .isNull();
+
+        for (MeasurementUnit measurementUnit : MeasurementUnit.values()) {
+            String measurementUnitName = measurementUnit.name();
+            assertThat(converter.convertToEntityAttribute(measurementUnitName))
+                    .isEqualByComparingTo(MeasurementUnit.valueOf(measurementUnitName));
+        }
+    }
+}
