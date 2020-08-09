98352418a4d4553ef95314167a252ca30e15e340
Gustavo Mendes Maciel <gmmaciel.bh2@gmail.com>
2020-08-09 12:47:35 -0300

    Added findByName to IngredientRepository

--- src/main/java/com/mergipe/recipesapp/ingredient/IngredientRepository.java
+++ src/main/java/com/mergipe/recipesapp/ingredient/IngredientRepository.java
@@ -3,4 +3,6 @@ package com.mergipe.recipesapp.ingredient;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
+
+    Ingredient findByName(String name);
 }
